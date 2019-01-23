4926a00ff4d00e018932484a1eb66e2ce223c93a
Goeun Park <goeuntothepark@gmail.com>
2019-01-22 16:03:51 -0800

    normalize the polygons so they're not too sharp

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,25 +2,38 @@ const svgWidth = 1000;
 const svgHeight = 700;
 let numLandforms = [1, 2, 3, 4, 5];
 numLandforms = numLandforms[Math.floor(Math.random() * numLandforms.length)];
-let landSize = [300, 400, 800, 1000, 1200, 1500];
+let landSize = [300, 400, 800, 1000, 1200, 1500, 1500, 2000];
+let landDensity = [10, 500, 1000];
 const randomX = d3.randomUniform(0, svgWidth);
 const randomY = d3.randomUniform(0, svgHeight);
 const data = d3.range(50000).map(function() { return [randomX(), randomY()]; });
-const delaunay = d3.Delaunay.from(data);
-const voronoi = delaunay.voronoi([0, 0, svgWidth, svgHeight]);
 const num = data.length;
+let delaunay = d3.Delaunay.from(data);
+let voronoi = delaunay.voronoi([0, 0, svgWidth, svgHeight]);
+let relaxedPoints;
+
+// normalize points of polygons
+for (let i = 0; i < 2; i++){
+  relaxedPoints = voronoi.cellPolygons();
+  relaxedPoints = [...relaxedPoints];
+  relaxedPoints = relaxedPoints.map(x => d3.polygonCentroid(x));
+}
+delaunay = d3.Delaunay.from(relaxedPoints);
+voronoi = delaunay.voronoi([0, 0, svgWidth, svgHeight]);
+
+
 
 let svg = d3.select('svg')
-  .attr("id", "map")
-  .attr('width', svgWidth)
-  .attr('height', svgHeight)
-  .append("g")
-  .call(d3.zoom()
-    .scaleExtent([1, 50])
-    .translateExtent([[0, 0], [svgWidth, svgHeight]])
-    .on("zoom", function () {
-      svg.attr("transform", d3.event.transform)
-    }));
+.attr("id", "map")
+.attr('width', svgWidth)
+.attr('height', svgHeight)
+.append("g")
+.call(d3.zoom()
+.scaleExtent([1, 50])
+.translateExtent([[0, 0], [svgWidth, svgHeight]])
+.on("zoom", function () {
+  svg.attr("transform", d3.event.transform)
+}));
 
 // locationNeighbor key: seed point, value: array of neighboring points
 let locationNeighbors = {};
@@ -38,8 +51,7 @@ const createNeighbors = function createNeighbors() {
   for (let location in locationNeighbors) {
     let expansion = locationNeighbors[location];
     let neighborFactor = landSize[Math.floor(Math.random() * landSize.length)];
-    let constrictingFactor = Math.floor(Math.random() * 1000) + 5;
-    console.log(constrictingFactor)
+    let constrictingFactor = landDensity[Math.floor(Math.random() * landDensity.length)];
 
     for (let i = 0; i < neighborFactor; i++) {
       let edges = expansion.slice(Math.max(expansion.length - constrictingFactor));
